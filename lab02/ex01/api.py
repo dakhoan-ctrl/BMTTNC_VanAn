a1fccc4141c19658895e6a61b3456c95277d7573
dakhoan-ctrl <dakhoan@gmail.com>
2026-03-09 15:05:04 +0700

    lab02

--- lab02/ex01/api.py
+++ lab02/ex01/api.py
@@ -1,25 +1,32 @@
 from flask import Flask, request, jsonify
-# Import thuật toán Caesar
+# Import các thuật toán cũ
 from cipher.caesar.caesar_cipher import CaesarCipher
-# Import thuật toán Vigenere
 from cipher.vigenere.vigenere_cipher import VigenereCipher
+from cipher.playfair import PlayFairCipher
+from cipher.railfence import RailFenceCipher
 
+# --- THÊM DÒNG NÀY: Import thuật toán Transposition ---
+from cipher.transposition.transposition_cipher import TranspositionCipher
+
+# Khởi tạo app Flask
 app = Flask(__name__)
 
 # Khởi tạo các đối tượng cipher
 caesar_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
+playfair_cipher = PlayFairCipher()
+railfence_cipher = RailFenceCipher()
+
+# --- THÊM DÒNG NÀY: Khởi tạo đối tượng Transposition ---
+transposition_cipher = TranspositionCipher()
 
 # --- ROUTES CHO CAESAR CIPHER ---
-
 @app.route('/api/caesar/encrypt', methods=['POST'])
 def caesar_encrypt():
     data = request.json
     plain_text = data.get('plain_text')
     key = int(data.get('key'))
-    
     encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
-    
     return jsonify({'encrypted_message': encrypted_text})
 
 @app.route('/api/caesar/decrypt', methods=['POST'])
@@ -27,21 +34,16 @@ def caesar_decrypt():
     data = request.json
     cipher_text = data.get('cipher_text')
     key = int(data.get('key'))
-    
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
-    
     return jsonify({'decrypted_message': decrypted_text})
 
 # --- ROUTES CHO VIGENERE CIPHER ---
-
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data = request.json
     plain_text = data.get('plain_text')
     key = data.get('key')
-    
     encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
-    
     return jsonify({'encrypted_message': encrypted_text})
 
 @app.route('/api/vigenere/decrypt', methods=['POST'])
@@ -49,10 +51,73 @@ def vigenere_decrypt():
     data = request.json
     cipher_text = data.get('cipher_text')
     key = data.get('key')
-    
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
-    
     return jsonify({'decrypted_message': decrypted_text})
 
+# --- ROUTES CHO PLAYFAIR CIPHER ---
+@app.route('/api/playfair/creatematrix', methods=['POST'])
+def playfair_creatematrix():
+    data = request.json
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({"playfair_matrix": playfair_matrix})
+
+@app.route('/api/playfair/encrypt', methods=['POST'])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods=['POST'])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# --- ROUTES CHO RAIL FENCE CIPHER ---
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def railfence_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def railfence_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# --- 4. THÊM ROUTES CHO TRANSPOSITION CIPHER TẠI ĐÂY ---
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.json
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    # Gọi hàm encrypt từ class TranspositionCipher
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.json
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    # Gọi hàm decrypt từ class TranspositionCipher
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+# --- KẾT THÚC PHẦN THÊM MỚI ---
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
